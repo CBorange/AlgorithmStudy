bc2e98013d398108e88b2d067543e2616323d35e
DESKTOP-6936PDD\xocns <xocnsdl2013@gmail.com>
2023-05-18 22:40:52 +0900

    해시 테스트 추가

--- src/com/study/Main.java
+++ src/com/study/Main.java
@@ -6,7 +6,21 @@ public class Main {
         String a = "asfasf";
         String b = "wqrqwrwq";
 
-	    System.out.println(a.hashCode());
-        System.out.println(b.hashCode());
+        int hashA = a.hashCode();
+        int hashB = b.hashCode();
+
+	    System.out.println("hashA Integer : " + hashA);
+        System.out.println("hashA Binary : " + Integer.toBinaryString(hashA));
+        System.out.println("hashB Integer : " + hashB);
+        System.out.println("hashB Binary : " + Integer.toBinaryString(hashB));
+
+        hashA = hashA & 0x7FFFFFFF;
+        hashB = hashB & 0x7FFFFFFF;
+
+        System.out.println("hashA Positive Integer : " + hashA);
+        System.out.println("hashA Positive Binary : " + Integer.toBinaryString(hashA));
+
+        System.out.println("hashB Positive Integer : " + hashB);
+        System.out.println("hashB Positive Binary : " + Integer.toBinaryString(hashB));
     }
 }
