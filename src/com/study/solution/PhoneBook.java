b7834e05398ed73d5d0f91abccf9ede9261a8e87
CBorange <31188689+CBorange@users.noreply.github.com>
2023-08-07 18:00:03 +0900

    전화번호부 솔루션 추가
    전화번호부 솔루션 추가

--- src/com/study/solution/PhoneBook.java
+++ src/com/study/solution/PhoneBook.java
@@ -2,16 +2,21 @@ package com.study.solution;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class PhoneBook {
     public boolean solution(String[] phone_book) {
-        ArrayList<String> list = new ArrayList<>(phone_book.length);
-        list.addAll(List.of(phone_book));
-        Collections.sort(list);
+        HashMap<String, Boolean> map = new HashMap<>();
+        for(int i=0; i < phone_book.length; ++i){
+            String current = phone_book[i];
+            String fullString = "";
+            for(int j=0; j < current.length(); ++i){
+                char temp = current.charAt(j);
+            }
 
-        boolean answer = true;
-        return answer;
+        }
+        return true;
     }
 }
 
